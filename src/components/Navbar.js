c277ba8431085ca77f50f0823e9ad6bd8128a274
Shishir Gurung <Shishirgrg12c@gmail.com>
2020-06-07 00:07:25 +0545

    added basic pages and navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import logo from "../images/long.svg";
+import { FaAlignRight } from "react-icons/fa";
+
+export default class Navbar extends Component {
+  render() {
+    return <div>Hello from Navbar</div>;
+  }
+}
