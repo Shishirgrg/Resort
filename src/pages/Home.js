e2415abde1ba3a8ebd0edfa63b6f6a54d4223703
Shishir Gurung <Shishirgrg12c@gmail.com>
2020-06-08 16:38:30 +0545

    banner component rendered in different pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <Hero>
+      <Banner title='luxurious room' subtitle='deluxe room starting at $299'>
+        <Link to='/rooms' className='btn-primary'>
+          view rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
