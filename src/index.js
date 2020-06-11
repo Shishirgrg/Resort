39bf1f7666fa491c85d8dcacdfe357c2b3ae5463
Shishir Gurung <Shishirgrg12c@gmail.com>
2020-06-11 23:00:59 +0545

    context api setup

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 // import './index.css';
 import App from "./App";
+import { RoomProvider } from "./context";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <RoomProvider>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </RoomProvider>,
   document.getElementById("root")
 );
 
