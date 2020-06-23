956d94770908741786e084c5a85f10a391e37be6
Shishir Gurung <Shishirgrg12c@gmail.com>
2020-06-23 22:03:49 +0545

    featured room created

---
+++ src/components/FeaturedRoom.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { RoomContext } from "../context";
+import Loading from "./Loading";
+
+export default class FeaturedRoom extends Component {
+  static contextType = RoomContext;
+
+  render() {
+    const { featuredRooms: rooms } = this.context;
+    console.log(rooms);
+
+    return (
+      <div>
+        Hello from featured rooms
+        <Loading />
+      </div>
+    );
+  }
+}
